d11732e750c7f0e5c507b0db5d035fd372408eb2
Durga <Durga@grctechllc.com>
2025-10-18 15:11:28 +0530

    new changes

--- my-portfolio/src/app.tsx
+++ my-portfolio/src/app.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "./components/pages/layout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
